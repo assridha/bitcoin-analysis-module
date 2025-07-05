d62e2cff589182a82b7f13deb2bfe9d05aa95a39
Ashwin Sridhar <ashwin.sridhar.a@gmail.com>
2025-07-05 16:33:22 +0200

    Add arch dependency back and fix pandas warnings

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='bitcoin_analysis',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'pandas==2.2.3',
         'numpy==1.26.4',
+        'arch==5.3.1',
     ],
     py_modules=['bitcoin_analysis'],
 ) 
